0bda421f519dd7f859dc040561087c4dbd491df0
ssj4dofuturo <felip3.fl@gmail.com>
2021-06-18 18:33:14 -0300

    06 Implementação Service

---
+++ source/backend/Adt.Challenge.Business/Services/RestaurantService.cs
@@ -0,0 +1,50 @@
+﻿using Adt.Challenge.Business.Interfaces.Repositories;
+using Adt.Challenge.Business.Interfaces.Services;
+using Adt.Challenge.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adt.Challenge.Business.Services
+{
+    public class RestaurantService : IRestaurantService
+    {
+        private readonly IRestaurantRepository _restaurantRepository;
+
+        public RestaurantService(IRestaurantRepository restaurantRepository)
+        {
+            _restaurantRepository = restaurantRepository;
+        }
+
+        public Task<List<Restaurant>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetNameByHourMinute(string hourMinute)
+        {
+            try
+            {
+                var hourMinuteTime = Convert.ToDateTime(hourMinute).TimeOfDay;
+                return this.GetNameByHourMinute(hourMinuteTime);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+
+        public Task<List<string>> GetNameByHourMinute(TimeSpan hourMinute)
+        {
+            try
+            {
+                return _restaurantRepository.GetNameByHourMinute(hourMinute);
+            }
+            catch (Exception ex)
+            {   
+                throw new ArgumentException(ex.Message);
+            }
+        }
+    }
+}
---
+++ source/backend/Adt.Challenge.Business/Services/ServiceBase.cs
@@ -0,0 +1,16 @@
+﻿using Adt.Challenge.Business.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adt.Challenge.Business.Services
+{
+    public class ServiceBase<T> : IServiceBase<T> where T : class
+    {
+        public Task<List<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
