0121823a8651224be0a525bc3fbdee478b23e94e
ssj4dofuturo <felip3.fl@gmail.com>
2021-06-18 22:38:57 -0300

    09 Criação de teste unitário para o Controllers

--- source/backend/Adt.Challenge.Tests/Presentation/Api/RestaurantControllerTests.cs
+++ source/backend/Adt.Challenge.Tests/Presentation/Api/RestaurantControllerTests.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Adt.Challenge.Api.Controllers.V1;
+using Adt.Challenge.Business.Interfaces.Services;
+using Moq;
+using Xunit;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Adt.Challenge.Business.Models;
 
 namespace Adt.Challenge.Tests.Presentation.Api
 {
     public class RestaurantControllerTests
     {
+        [Fact]
+        public void Should_Create_New_Object()
+        {
+            //arrange
+            //act
+            var controller = new RestaurantController(Mock.Of<IRestaurantService>());
 
+            //assert
+            controller.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task InvalidHourMinute_ShouldReturn_NotFound()
+        {
+            //arrange
+            var hourMinute = "00";
+            var restaurantController = new RestaurantController(Mock.Of<IRestaurantService>());
+
+            //act
+            var result = await restaurantController.GetNameByHourMinute(hourMinute);
+
+            //assert
+            result
+                .Should().BeOfType<NotFoundObjectResult>()
+                .Which.Value.Should().Be(hourMinute);
+        }
     }
 }
